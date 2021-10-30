f06f116aca83d721bf591d1149a2736401612450
Cesar Covarrubias <ccovarrubias1@student.gsu.edu>
2021-10-30 03:27:25 +0000

    final commit milestone3

--- app.py
+++ app.py
@@ -6,24 +6,21 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-
 from flask_sqlalchemy import SQLAlchemy
 
 import os
 from genius import get_lyrics_link
 from spotify import get_access_token, get_song_data
 
-import flask
 from flask_login import login_user, current_user, LoginManager
-from flask_login.utils import login_required
 
 import random
 import base64
 import requests
 
-#damp-harbor-67431
+# damp-harbor-67431
 
-app = flask.Flask(__name__, static_folder='./build/static')
+app = flask.Flask(__name__, static_folder="./build/static")
 # Point SQLAlchemy to your Heroku database
 db_url = os.getenv("DATABASE_URL")
 if db_url.startswith("postgres://"):
@@ -61,22 +58,53 @@ class Artist(db.Model):
 
 db.create_all()
 
-# This tells our Flask app to look at the results of `npm build` instead of the 
+# This tells our Flask app to look at the results of `npm build` instead of the
 # actual files in /templates when we're looking for the index page file. This allows
 # us to load React code into a webpage. Look up create-react-app for more reading on
 # why this is necessary.
 bp = flask.Blueprint("bp", __name__, template_folder="./build")
 
-@bp.route('/index')
+
+@bp.route("/index")
 @login_required
 def index():
     # TODO: insert the data fetched by your app main page here as a JSON
-    DATA = {"your": "data here"}
+    artists = Artist.query.filter_by(username=current_user.username).all()
+    artist_ids = [a.artist_id for a in artists]
+    has_artists_saved = len(artist_ids) > 0
+    if has_artists_saved:
+        artist_id = random.choice(artist_ids)
+
+        # API calls
+        access_token = get_access_token()
+        (song_name, song_artist, song_image_url, preview_url) = get_song_data(
+            artist_id, access_token
+        )
+        genius_url = get_lyrics_link(song_name)
+    else:
+        (song_name, song_artist, song_image_url, preview_url, genius_url) = (
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
+
+    DATA = {
+        "artist_ids": artist_ids,
+        "has_artists_saved": has_artists_saved,
+        "song_name": song_name,
+        "song_artist": song_artist,
+        "song_image_url": song_image_url,
+        "preview_url": preview_url,
+        "genius_url": genius_url,
+    }
     data = json.dumps(DATA)
     return flask.render_template(
         "index.html",
         data=data,
     )
+
 
 app.register_blueprint(bp)
 
@@ -158,6 +186,15 @@ def save():
     return flask.redirect(flask.url_for("bp.index"))
 
 
+@app.route("/delete", methods=["POST"])
+def delete():
+    artist_id = flask.request.form.get("artist_id")
+    username = current_user.username
+    db.session.delete(Artist(artist_id=artist_id, username=username))
+    db.session.commit()
+    return flask.redirect(flask.url_for("bp.index"))
+
+
 @app.route("/")
 def main():
     if current_user.is_authenticated:
@@ -165,46 +202,8 @@ def main():
     return flask.redirect(flask.url_for("login"))
 
 
-@app.route("/foo")
-@login_required
-def foo():
-    artists = Artist.query.filter_by(username=current_user.username).all()
-    artist_ids = [a.artist_id for a in artists]
-    has_artists_saved = len(artist_ids) > 0
-    if has_artists_saved:
-        artist_id = random.choice(artist_ids)
-
-        # API calls
-        access_token = get_access_token()
-        (song_name, song_artist, song_image_url, preview_url) = get_song_data(
-            artist_id, access_token
-        )
-        genius_url = get_lyrics_link(song_name)
-
-    else:
-        (song_name, song_artist, song_image_url, preview_url, genius_url) = (
-            None,
-            None,
-            None,
-            None,
-            None,
-        )
-
-    return flask.render_template(
-        "index.html",
-        has_artists_saved=has_artists_saved,
-        song_name=song_name,
-        song_artist=song_artist,
-        song_image_url=song_image_url,
-        preview_url=preview_url,
-        genius_url=genius_url,
-    )
-
-
-
-
-
 app.run(
-    #host=os.getenv('IP', '0.0.0.0'),
-    #port=int(os.getenv('PORT', 8081)),
+    debug=True
+    # host=os.getenv('IP', '0.0.0.0'),
+    # port=int(os.getenv('PORT', 8081)),
 )
--- src/App.js
+++ src/App.js
@@ -7,7 +7,61 @@ function App() {
   // fetches JSON data passed in by flask.render_template and loaded
   // in public/index.html in the script with id "data"
   const args = JSON.parse(document.getElementById("data").text);
+  const [numClicks, setNumClicks] = useState(0);
 
+  function onButtonClick() {
+    console.log(JSON.stringify({ "num_clicks": numClicks }));
+    fetch('/increment', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ "num_clicks": numClicks }),
+    }).then(response => response.json()).then(data => {
+      console.log(data);
+      setNumClicks(data.num_clicks_server);
+    })
+  }
+
+  return (
+    <>
+      <h1>Cesar's Song Explorer</h1>
+
+      {args.has_artists_saved ? (
+        <>
+          <h2>{args.song_name}</h2>
+          <h3>{args.song_artist}</h3>
+          <div>
+            <img src={args.song_image_url} width={300} height={300} />
+          </div>
+          <div>
+            <audio controls>
+              <source src={args.preview_url} />
+            </audio>
+          </div>
+          <a href={args.genius_url}> Click here to see lyrics! </a>
+          <h1>Your saved artists:</h1>
+          <h2> {args.artist_ids[0]}</h2>
+          <h3> {args.artist_ids[1]}</h3>
+          <h4> {args.artist_ids[2]}</h4>
+          <h5> {args.artist_ids[3]}</h5>
+
+          <form method="POST" action="/delete">
+            <input type="text" name="artist_id" />
+            <input type="submit" value="Delete" />
+          </form>
+
+        </>) :
+        (<h2>Looks like you don't have anything saved! Use the form below!</h2>)
+      }
+
+      <h1>Save a favorite artist ID for later:</h1>
+      <form method="POST" action="/save">
+        <input type="text" name="artist_id" />
+        <input type="submit" value="Save" />
+      </form>
+    </>
+  );
   // TODO: Implement your main page as a React component.
 }
 
